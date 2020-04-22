ed4fd3a1eb2cadee4b7ba700a5efc92a25be1842
seonuk <sunwook317@gmail.com>
2020-04-22 22:08:30 +0900

    spring boot2 & kotlin 개발 환경 설정

---
+++ src/main/kotlin/edu/donnguk/davinci/Application.kt
@@ -0,0 +1,13 @@
+package edu.donnguk.davinci
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+    println("hello")
+
+}
